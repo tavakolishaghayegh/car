ed1155bb6877616e7454975c062b2f3cb7d592aa
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-09-14 00:37:15 +0330

    Keyboard Input & if Statements

--- Assets/driver.cs
+++ Assets/driver.cs
@@ -1,19 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class driver : MonoBehaviour
 {
-    float steerSpeed = 0.5f;
-    float moveSpeed = 0.05f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        transform.Rotate(0, 0, 45);
-    }
+    [SerializeField]float steerSpeed = 2f;
+    [SerializeField]float moveSpeed = 0.1f;
+   
     
 
-    // Update is called once per frame
+   
     void Update()
     {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing forward");
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing backwards");
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+        }
+
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
+
+        
+
+
+
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0 , moveSpeed, 0);
     }
