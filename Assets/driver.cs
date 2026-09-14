a294d7cad75b6f05149808c1d351d7daa6c2f378
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-09-14 22:47:04 +0330

    Moving Our Car

--- Assets/driver.cs
+++ Assets/driver.cs
@@ -2,40 +2,44 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class driver : MonoBehaviour
-{
-    [SerializeField]float steerSpeed = 2f;
+{ 
     [SerializeField]float moveSpeed = 0.1f;
-   
-    
+
+    [SerializeField]float steerSpeed = 2f;
 
    
     void Update()
     {
+        float move=0f;
+        float steer=0f;
+       
+
+
         if (Keyboard.current.wKey.isPressed)
         {
-            Debug.Log("We are pushing forward");
-        }
+            move = 1f;
+            }
 
         else if (Keyboard.current.sKey.isPressed)
         {
-            Debug.Log("We are pushing backwards");
+            move = -1f;
         }
 
         if (Keyboard.current.aKey.isPressed)
         {
-            Debug.Log("We are pushing left");
+            steer = 1f;
         }
 
         else if (Keyboard.current.dKey.isPressed)
         {
-            Debug.Log("We are pushing right");
+            steer= -1f;
         }
 
         
 
 
-
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0 , moveSpeed, 0);
+        transform.Translate(0 , move * moveSpeed, 0);
+        transform.Rotate(0, 0, steer * steerSpeed);
+        
     }
 }
