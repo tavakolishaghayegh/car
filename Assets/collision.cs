78a81b86e18e3bddfccbad6250bc2a3e3365be87
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-09-15 19:23:12 +0330

    Using OnCollisionEnter2D

---
+++ Assets/collision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision )
+    {
+        Debug.Log("akh");
+    }
+        
+    
+
+
+
+}
