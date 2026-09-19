a75ce5e16f8169319c1b991d5adc8cdabdb657ab
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-09-19 22:46:59 +0330

    Introducing Bools

--- Assets/delivery.cs
+++ Assets/delivery.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class delivery : MonoBehaviour
 {
+    bool hasPackage;
+  
 
-    void OnTriggerEnter2D(Collider2D collision) 
+
+
+  void OnTriggerEnter2D(Collider2D collision) 
     {
-        // if (the tag is Package)
-        // then (print picked up Package to console)
+        
         if(collision.CompareTag("Package"))
         {
              Debug.Log("picked up Package");
+             hasPackage= true;
         }
 
-        if(collision.CompareTag("customer"))
+        if(collision.CompareTag("customer") && hasPackage)
         {
              Debug.Log("Deliverd package");
+             hasPackage= false;
         }
        
 
